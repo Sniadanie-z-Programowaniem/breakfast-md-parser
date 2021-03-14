4e5ce8bd4d1db254abdd8adf7fdc67dd437a00c4
Michal Michalczuk <michalczukm@gmail.com>
2021-03-14 18:45:11 +0100

    add extra tests for news parser, improve news parser

--- src/parser/episode/__tests__/parse-news.test.ts
+++ src/parser/episode/__tests__/parse-news.test.ts
@@ -39,6 +39,92 @@ describe('parse-news', () => {
         expect(actual).toEqual(expected);
     });
 
+    it('should parse when news url as sub-list, with multiple links', async () => {
+        const md = `
+- Open Web Docs
+  - https://web.dev/open-web-docs/
+  - https://opencollective.com/open-web-docs
+  - https://hacks.mozilla.org/2021/01/welcoming-open-web-docs-to-the-mdn-family/
+  - https://opencollective.com/open-web-docs/updates/introducing-open-web-docs
+`;
+        const expected: NewsToken = {
+            title: 'Open Web Docs',
+            description: '',
+            links: [
+                'https://web.dev/open-web-docs/',
+                'https://opencollective.com/open-web-docs',
+                'https://hacks.mozilla.org/2021/01/welcoming-open-web-docs-to-the-mdn-family/',
+                'https://opencollective.com/open-web-docs/updates/introducing-open-web-docs',
+            ],
+        };
+
+        const actual = await parseNews(await getListItemToken(md));
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should parse when has url as sub-list and contains single-line description', async () => {
+        const md = `
+- Blitz.js - The Fullstack React Framework
+  Blitz is a hyper-productive fullstack React framework that's built on Next.js and features a "Zero-API" data layer.
+
+  - https://blitzjs.com/
+`;
+        const expected: NewsToken = {
+            title: 'Blitz.js - The Fullstack React Framework',
+            description: `Blitz is a hyper-productive fullstack React framework that's built on Next.js and features a "Zero-API" data layer.`,
+            links: ['https://blitzjs.com/'],
+        };
+
+        const actual = await parseNews(await getListItemToken(md));
+
+        expect(actual).toEqual(expected);
+    });
+
+    it('should parse when has urls as sub-list and contains single-line description with multiple links', async () => {
+        const md = `
+- Gather.town - wirtualne biuro w formie gierki, czy to ma sens?
+  description description.
+
+  - http://gather.town
+  - https://news.ycombinator.com/item?id=25039370
+`;
+        const expected: NewsToken = {
+            title: 'Gather.town - wirtualne biuro w formie gierki, czy to ma sens?',
+            description: `description description.`,
+            links: ['http://gather.town', 'https://news.ycombinator.com/item?id=25039370'],
+        };
+
+        const actual = await parseNews(await getListItemToken(md));
+
+        expect(actual).toEqual(expected);
+    });
+
+    it.skip('should parse when has urls as sub-list and links have extra text', async () => {
+        const md = `
+- Microsoft ogłasza dodatkowy support dla Blazora w IDE + wychodzą darmowe biblioteki komponentów open source
+  Blazor nabiera tempa
+
+  - 2. Komponenty: https://visualstudiomagazine.com/articles/2021/01/21/radzen-open-source.aspx
+  - 1. IDE: https://visualstudiomagazine.com/articles/2021/01/26/razor-editor-updates.aspx
+  - 3. StackTrends: https://insights.stackoverflow.com/trends?tags=vue.js%2Cangular%2Cblazor
+`;
+        const expected: NewsToken = {
+            title:
+                'Microsoft ogłasza dodatkowy support dla Blazora w IDE + wychodzą darmowe biblioteki komponentów open source',
+            description: `Blazor nabiera tempa`,
+            links: [
+                'https://visualstudiomagazine.com/articles/2021/01/21/radzen-open-source.aspx',
+                'https://visualstudiomagazine.com/articles/2021/01/26/razor-editor-updates.aspx',
+                'https://insights.stackoverflow.com/trends?tags=vue.js%2Cangular%2Cblazor',
+            ],
+        };
+
+        const actual = await parseNews(await getListItemToken(md));
+
+        expect(actual).toEqual(expected);
+    });
+
     async function getListItemToken(content: string): Promise<Tokens.ListItem> {
         const listToken: Tokens.List | undefined = asTokens(await tokenize(content)).find(
             isListToken,
--- src/parser/episode/parse-news.ts
+++ src/parser/episode/parse-news.ts
@@ -5,32 +5,37 @@ import { BreakParsingError } from '../exceptions';
 import { NewsToken } from './types';
 import { Tokens } from 'marked';
 
-const whenTitleContainsLink = (titleTextTokens?: Tokens.DiscriminatedToken[]): NewsToken => {
-    const titleToken = titleTextTokens?.find(isTextToken);
-    const linkToken = titleTextTokens?.find(isLinkToken);
+const parseTitleAndDescriptionToken = (
+    tokens: Tokens.DiscriminatedToken[],
+): Pick<NewsToken, 'title' | 'description'> => {
+    const titleToken = tokens?.find(isTextToken);
 
     if (!titleToken) {
         throw new BreakParsingError('News title not found');
     }
+
+    const [title, ...descriptionLines] = titleToken.text.split(/\r?\n/);
+
+    return {
+        title,
+        description: descriptionLines.join(' '),
+    };
+};
+
+const whenTitleContainsLink = (titleTextTokens: Tokens.DiscriminatedToken[]): NewsToken => {
+    const linkToken = titleTextTokens?.find(isLinkToken);
 
     if (!linkToken) {
         throw new BreakParsingError('News link not found');
     }
 
     return {
-        title: titleToken.text.trim(),
-        description: '',
+        ...parseTitleAndDescriptionToken(titleTextTokens),
         links: [linkToken.href],
     };
 };
 
-const whenLinksAreSubList = (itemTokens?: Tokens.DiscriminatedToken[]): NewsToken => {
-    const titleToken = itemTokens?.find(isTextToken);
-
-    if (!titleToken) {
-        throw new BreakParsingError('News title not found');
-    }
-
+const whenLinksAreSubList = (itemTokens: Tokens.DiscriminatedToken[]): NewsToken => {
     const linksTokens = itemTokens
         ?.find(isListToken)
         ?.items.map(linkFromListItem)
@@ -41,15 +46,18 @@ const whenLinksAreSubList = (itemTokens?: Tokens.DiscriminatedToken[]): NewsToke
     }
 
     return {
-        title: titleToken.text.trim(),
-        description: '',
+        ...parseTitleAndDescriptionToken(itemTokens),
         links: linksTokens.map((link) => link.href),
     };
 };
 
 export const parseNews = (item: Tokens.ListItem): NewsToken => {
+    if (!item.tokens) {
+        throw new BreakParsingError('News root item does not contain any tokens');
+    }
+
     const titleTextTokens = item.tokens
-        ?.filter(isTextToken)
+        .filter(isTextToken)
         .flatMap((textToken) => textToken.tokens)
         .filter(isDefined);
 
